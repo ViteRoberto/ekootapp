cb4ed3a4f27e5eb3dafbcf66b8ebf6794e6655a7
ViteRoberto <roberto.vite@hotmail.com>
2018-12-04 23:52:57 -0600

    Creación de Slides sin Vincular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'acs', loadChildren: './acs/acs.module#AcsPageModule' },
   { path: 'empresas', loadChildren: './empresas/empresas.module#EmpresasPageModule' },
-  { path: 'modalReto', loadChildren: './modal-reto/modal-reto.module#ModalRetoPageModule' }
+  { path: 'modalReto', loadChildren: './modal-reto/modal-reto.module#ModalRetoPageModule' },
+  { path: 'tutorial', loadChildren: './tutorial/tutorial.module#TutorialPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
