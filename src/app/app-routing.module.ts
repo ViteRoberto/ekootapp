32203535adf3758888418cc5405a24193adc7acf
Roberto Vite <roberto.vite@hotmail.com>
2019-01-10 19:25:42 -0600

    trabajando con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './tutorial/tutorial.module#TutorialPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'acs', loadChildren: './acs/acs.module#AcsPageModule' },
   { path: 'empresas', loadChildren: './empresas/empresas.module#EmpresasPageModule' },
-  { path: 'modalReto', loadChildren: './modal-reto/modal-reto.module#ModalRetoPageModule' },
-  { path: 'tutorial', loadChildren: './tutorial/tutorial.module#TutorialPageModule' }
+  { path: 'modalReto', loadChildren: './modal-reto/modal-reto.module#ModalRetoPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
